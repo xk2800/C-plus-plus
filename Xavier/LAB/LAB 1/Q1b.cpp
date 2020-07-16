bec4cb36c8994b6fe648d0362e55ad5cc1d7a649
Xavier K <59243836+xk2800@users.noreply.github.com>
2020-07-16 16:24:29 +0800

    1b

---
+++ Xavier/LAB/LAB 1/Q1b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{     int x = 8, y = 2, z;
+cout << ++x << endl;
+cout << x << endl;
+cout << x-- << endl;
+cout << x + y << endl;
+//cout << x <<” “<< y << endl;
+cout << "x * x = "; 
+cout << x * x << endl;
+cout << y++ << endl;
+z = x % y;
+cout << z << endl;
+return 0;
+}
