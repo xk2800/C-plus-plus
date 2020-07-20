410b68ad978659f72f8a0608b12202271e0f4223
Xavier K <59243836+xk2800@users.noreply.github.com>
2020-07-21 01:44:43 +0800

    ..

---
+++ Xavier/LAB/LAB 1/Q1a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+
+    cout<<"Sales Report for September 15, 2010" << endl;
+    cout<<"Artist   Title  Price    Genre   Disc  Sale" << endl;
+    cout<<"Merle    Blue   12.99  Country    4%   12.47" << endl;
+    cout<<"Richard  Music   8.49  Classical  8%   7.81" << endl;
+
+
+
+
+
+}
