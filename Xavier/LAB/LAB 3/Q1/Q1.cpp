449d324532f3a9345203eb3b83246678a36f9672
Xavier K <59243836+xk2800@users.noreply.github.com>
2020-08-04 01:16:09 +0800

    LAB 3 Q1 few

---
+++ Xavier/LAB/LAB 3/Q1/Q1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+class TravelPackage
+{  public:
+    char package;
+    int noAdult, noChild;                                   
+    float adultPrice, childPrice, discount, totalPrice;
+
+};
+
+int main(){
+
+    do{
+
+
+        switch(){
+
+            
+
+
+        }
+
+
+    }while()
+
+
+
+
+
+
+
+
+
+}
