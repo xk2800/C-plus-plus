56c49fc5717d082a47e859f895b661575209bc5b
Xavier K <xavierkhew00@gmail.com>
2020-08-20 10:11:43 +0800

    trt

---
+++ Xavier/LAB/LAB 5/Q3/Q3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+class PreOrder{
+
+    string title;
+    double price;
+
+    public:
+        void BookSelection(){
+
+            cout<<"[1] Mum’s Favourite Recipes Book\t\tPrice: RM 14.00"<<endl;
+            cout<<"[2] Weight Watchers Everyday Favourite Recipes Book\tPrice: RM 12.00"<<endl;
+            cout<<"[3] The Skinny 5:2 Diet Family Recipes Book\tPrice: RM10.90"<<endl;
+            cout<<"[4] Favourite Teatime Recipes Book\t\tPrice: RM10.00"<<endl;
+            cout<<"[5] Favourite Traditional and Retro Recipes\t\tPrice: RM13.00"<<endl;
+        }
+
+};
