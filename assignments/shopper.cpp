1e3f9cf4089040860e4cf3d507d29ba25621117b
Xavier K <xavierkhew00@gmail.com>
2020-09-10 15:41:04 +0800

    update comments

--- assignments/shopper.cpp
+++ assignments/shopper.cpp
@@ -1,7 +1,7 @@
 /**********|**********|**********|
 Program: YOUR_FILENAME.cpp / YOUR_FILENAME.h
 Course: Object Oriented Programming
-Year: 2010/11 Trimester 1
+Year: 2020/21 Trimester 1
 Name: Khew Xavier Ethan
 ID: 1181202757
 Lecture Section: TC02
@@ -9,3 +9,4 @@ Tutorial Section: TT04
 Email: 1181202757@student.mmu.edu.my
 Phone: 010-2311245
 **********|**********|**********/
+
