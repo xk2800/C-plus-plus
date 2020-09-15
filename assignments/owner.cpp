6e0f94252ab0013485926241ba7122f08989bd2a
eltonwcm00 <eltonwcm00@gmail.com>
2020-09-15 19:03:59 +0800

    owner login

--- assignments/owner.cpp
+++ assignments/owner.cpp
@@ -17,20 +17,22 @@ Phone :                 01111207201
 #include<cstdlib>
 using namespace std;
 
+void registerLogic();
+void loginLogic();
 
-/* REGISTER CLASS */
+/*********************************************** REGISTER CLASS ***********************************************/
 
 class Register {
 	
 	private:
-		string username;
-		float pass, cfrmpass;
+		string oname;
+		string pass, cfrmpass;
 	
 	public:
 		
 		void usernameRegister() {
-			cout<<"Set a name : ";
-			getline(cin, username);
+			cout<<"Set a username : ";
+			getline(cin, oname);
 			cout<<endl;	
 		}
 		
@@ -42,7 +44,7 @@ class Register {
 		
 			while(pass != cfrmpass) {
 				cout<<"The password you entered is not matched ! Please try again"<<endl<<endl;
-				cout<<"Enter a password [0-9 digits only]: ";
+				cout<<"Enter a password : ";
 			    cin>>pass;
 			    cout<<"Re-enter the password : ";
 			    cin>>cfrmpass;	
@@ -50,30 +52,60 @@ class Register {
 		}
 		
 		string getUser() {
-			return username;
+			return oname;
 		}
 		
-		float getPass() {
+		string getPass() {
 			return pass;
 		}	
 		
-		float getCfrmpass() {
+		string getCfrmpass() {
 			return cfrmpass;
 		}
-		
 };
 
-int main() {
+/*********************************************** LOGIN CLASS ***********************************************/
+
+class Login {
 	
-	/* REGISTER LOGIC */
+	private:
+		string username;
+		string pass;
+		Register reg_friend_obj;
+		
+	public:
+		usernameLogin() {	
+			cout<<"Enter your username : ";
+			cin.ignore();
+			getline(cin, username);
+			cout<<endl;
+		}
+		
+		passLogin() {
+			cout<<"Enter your password : ";
+			cin>>pass;
+		}
+		
+		string getUser() {
+			return username;
+		}
+		
+		string getPass() {
+			return pass;
+		}
+};
+
+/*********************************************** registerLogic() ***********************************************/
+
+void registerLogic() {
 	
 	Register reg_obj;
 	ofstream out_reg_file;
 	
-	string uname;
-	float password, cfrmpassword;
+	string oname;
+	string password, cfrmpassword;
 	
-	out_reg_file.open("register.txt");
+	out_reg_file.open("owner-details.txt");
 	
 	if(!out_reg_file) {
 		cout<<"File is not found !"<<endl;
@@ -81,7 +113,7 @@ int main() {
 		reg_obj.usernameRegister();
 		reg_obj.passRegister();
 		
-		uname = reg_obj.getUser();
+		oname = reg_obj.getUser();
 		password = reg_obj.getPass();
 		cfrmpassword = reg_obj.getCfrmpass();
 		
@@ -92,8 +124,59 @@ int main() {
 		out_reg_file<<"Shop Owner Account Details"<<endl;
 		out_reg_file<<"=============================="<<endl;
 		out_reg_file<<"Username      Password      Confirm Password"<<endl;
-		out_reg_file<<uname<<setw(15)<<password<<setw(15)<<cfrmpassword<<endl;	
+		out_reg_file<<oname<<setw(15)<<password<<setw(15)<<cfrmpassword<<endl;	
 	}
 			
-	out_reg_file.close();		
+	out_reg_file.close();
+}
+
+/*********************************************** loginLogic() ***********************************************/
+
+void loginLogic() {
+	
+	Login log_obj;
+	ifstream in_log_file;
+
+	string pass, password;
+	int offset;
+	bool validate;
+	
+	in_log_file.open("owner-details.txt");
+	
+	log_obj.passLogin();
+	pass = log_obj.getPass();
+	cout<<endl;
+	
+	if(in_log_file.is_open()) {
+		cout<<"Reading from the file...."<<endl<<endl; 
+	
+		while(!in_log_file.eof()) {
+			getline(in_log_file, password);
+		
+			if((offset = password.find(pass, 0))!= string::npos) {	
+				validate = true;
+				break;
+			} else {
+				validate = false;	
+			}
+		}
+		
+		if(validate == true) {
+			cout<<"Successfully";
+		} else {
+			cout<<"Password incorrect ! Please try again"<<endl;	
+		}
+	} 
+	
+	in_log_file.close();
+}
+
+/*********************************************** int main() ***********************************************/
+
+int main() {
+	
+	registerLogic();
+	loginLogic();
+		
+	return 0;
 }
