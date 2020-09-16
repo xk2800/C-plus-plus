0e468aaa39c2b874858d92997ace43286e488bfe
Xavier K <xavierkhew00@gmail.com>
2020-09-17 03:15:07 +0800

    redesign login and reg code for shopper

--- assignments/xavier-test-rollout.cpp
+++ assignments/xavier-test-rollout.cpp
@@ -1,15 +1,3 @@
-/**********|**********|**********|
-Program             : shopper.cpp
-Course              : Object Oriented Programming
-Year                : 2020/21 Trimester 1
-Name                : Khew Xavier Ethan
-ID                  : 1181202757
-Lecture Section     : TC02
-Tutorial Section    : TT04
-Email               : 1181202757@student.mmu.edu.my
-Phone               : 010-2311245
-**********|**********|**********/
-
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -228,42 +216,6 @@ void mainmenu(){
         break;
     }
 }
-
-
-class customer{
-
-    private:
-        string name, address, password, id;
-        int cust_type;
-    
-    public:
-
-        void input(){
-
-            
-        }
-
-};
-
-class student{
-    
-    private:
-        string name, address, password, id;
-        int cust_type;
-    
-    public: 
-
-};
-
-class staff{
-
-    private:
-        string name, address, password, id;
-        int cust_type;
-    
-    public: 
-};
-
 
 int main(){
 
