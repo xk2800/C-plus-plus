fd7085d5575e3afac434837c152b21545cc6b331
Xavier K <xavierkhew00@gmail.com>
2020-09-16 23:54:33 +0800

    wow GCL

---
+++ Xavier/LAB/LAB 10/Q3/Q3.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+
+class Numbers
+{
+   private:
+      int* numbersOriginal;
+      int size;
+  
+   public:
+      void displayOriginal(){
+
+
+      }
+
+      void getNumbers()[
+
+
+      ]
+
+      void displaySorted(){
+
+          
+      }
+};
+
+
+//
+// If needed, declare functions here
+//
+
+
+
+int main()
+{
+   Numbers a;
+   cout << "Initial values by the default constructor :";
+   a.displayOriginal();
+   cout << endl;
+   
+   a.getNumbers();
+   cout << "Display in the ascending order : ";
+   a.displaySorted();
+   cout << endl;
+   cout << "Display in the original order : ";
+   a.displayOriginal();
+   cout << endl;
+   
+   return 0;
+
+}
+
+
+// Definition of class member functions from here
+
+....
+
+// If needed, definition of functions from here
